20a83439fa4ee0bc54a10cade4bbdd2ecaaeada5
kbtanvir <tanvirkhaan004@gmail.com>
2024-01-17 21:38:22 +0600

    light theme used

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
-import { BreakpointIndicator } from "@/components/breakpoint-indicator";
-import { Layout } from "@/components/layout";
-import { Toaster } from "@/components/ui/toaster";
-import { persistStore } from "@/lib/persist";
-import { cartStore } from "@/lib/persist/cart";
-import { Inter as FontSans } from "@next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
-import { type AppType } from "next/app";
-import "~/styles/globals.css";
-import { api } from "~/utils/api";
-
+import { BreakpointIndicator } from "@/components/breakpoint-indicator"
+import { Layout } from "@/components/layout"
+import { Toaster } from "@/components/ui/toaster"
+import { persistStore } from "@/lib/persist"
+import { cartStore } from "@/lib/persist/cart"
+import { Inter as FontSans } from "@next/font/google"
+import { Analytics } from "@vercel/analytics/react"
+import { type Session } from "next-auth"
+import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from "next-themes"
+import { type AppType } from "next/app"
+import "~/styles/globals.css"
+import { api } from "~/utils/api"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -33,7 +32,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
             --font-sans: ${fontSans.style.fontFamily};
           }
         `}</style>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Layout>
             <Component {...pageProps} />
           </Layout>
